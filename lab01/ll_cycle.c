3cdb7ca44e0a40e76934a5e0557fe8208871cbc8
JunLang-7 <1448760937@qq.com>
2025-02-28 13:26:52 +0800

    Finish lab01

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -3,5 +3,21 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    node* tortoise = head;
+    node* hare = head;
+    while (tortoise && hare) {
+        hare = hare->next;
+        if (hare == NULL) {
+            return 0;
+        }
+        hare = hare->next;
+        tortoise = tortoise->next;
+        if (hare == NULL || tortoise == NULL) {
+            return 0; 
+        }
+        if (hare == tortoise) {
+            return 1;
+        }
+    }
     return 0;
 }
